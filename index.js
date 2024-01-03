ced6b9ee93922687fd892332744cfc12e3d38099
Kewal Rathod <kewalrathod8@gmail.com>
2024-01-03 23:35:59 +0530

    * Fixed issues with Database.

--- index.js
+++ index.js
@@ -8,33 +8,57 @@ const app = express()
 const port = 5000
 
 app.use(express.json())
+const TEST = true
 
-// Get commit by id
+async function fetchParentCommit(owner, repository, headOid) {
+  const githubApiUrl = `https://api.github.com/repos/${owner}/${repository}/commits/${headOid}`
+
+  try {
+    const response = TEST ? "" : await axios.get(githubApiUrl)
+    const data = TEST ? baseGitResponse : response.data // baseGitResponse // response.data
+
+    if (data.parents && data.parents.length > 0) {
+      return data.parents[0].sha
+    } else {
+      console.warn("No Parent")
+    }
+  } catch (error) {
+    console.error("Error :", error.message)
+  }
+}
+
+// Get a commit by ID
 app.get("/repositories/:owner/:repository/commits/:oid", async (req, res) => {
   const { owner, repository, oid } = req.params
   const githubApiUrl = `https://api.github.com/repos/${owner}/${repository}/commits/${oid}`
+  const parentOid = await fetchParentCommit(owner, repository, oid)
 
   console.info("Data :", githubApiUrl)
 
   try {
-    // const response = await axios.get(githubApiUrl)
-    const { sha, commit, parents } = baseGitResponse // response.data
+    const response = TEST ? "" : await axios.get(githubApiUrl)
+    const { sha, commit, parents, committer, author } = TEST ? baseGitResponse : response.data // baseGitResponse // response.data
 
     const formatDate = (dateString) => dayjs(dateString).format("YYYY-MM-DDTHH:mm:ssZ")
 
     const result = [
       {
         oid: sha,
+        pid: parentOid,
         message: commit.message,
         author: {
           name: commit.author.name,
           date: formatDate(commit.author.date),
           email: commit.author.email,
+          avatar: author.avatar_url,
+          username: author.login,
         },
         committer: {
           name: commit.committer.name,
           date: formatDate(commit.committer.date),
           email: commit.committer.email,
+          avatar: committer.avatar_url,
+          username: committer.login,
         },
         parents: parents.map((parent) => ({
           oid: parent.sha,
@@ -49,23 +73,7 @@ app.get("/repositories/:owner/:repository/commits/:oid", async (req, res) => {
   }
 })
 
-// Difference
-async function fetchParentCommit(owner, repository, headOid) {
-  const githubApiUrl = `https://api.github.com/repos/${owner}/${repository}/commits/${headOid}`
-
-  try {
-    // const response = await axios.get(githubApiUrl)
-    const data = baseGitResponse // response.data
-
-    if (data.parents && data.parents.length > 0) {
-      return data.parents[0].sha
-    } else {
-      console.warn("No Parent")
-    }
-  } catch (error) {
-    console.error("Error :", error.message)
-  }
-}
+// Get the diff of a commit
 
 const parsePatch = (patchString) => {
   const lines = patchString.split("\n")
@@ -77,9 +85,10 @@ const parsePatch = (patchString) => {
 
   for (const line of lines) {
     if (line.startsWith("@@")) {
-      // Start of a new hunk
-      const header = line.substring(3).trim()
-      const numbers = header.match(/\-(\d+),?|\+(\d+),?/)
+      const header = line
+      const numbers = header.match(/-?(\d+),\d+ \+(\d+),\d+/)
+
+      console.info("Header :", header)
 
       baseLineNumber = numbers ? parseInt(numbers[1]) || 1 : null
       headLineNumber = numbers ? parseInt(numbers[2]) || baseLineNumber : null
@@ -87,31 +96,28 @@ const parsePatch = (patchString) => {
       currentHunk = { header, lines: [] }
       hunks.push(currentHunk)
     } else if (line.startsWith("-") || line.startsWith("+") || line.startsWith(" ")) {
-      // Line inside the hunk
       const lineType = line[0]
-      const content = line.substring(1).trim()
-
+      const content = line
       currentHunk.lines.push({ baseLineNumber, headLineNumber, content })
-
-      // Increment line numbers
       if (lineType === " " || lineType === "-") {
         baseLineNumber++
       }
       if (lineType === " " || lineType === "+") {
         headLineNumber++
-      }
+      } 
     }
   }
 
-  return { patch: { hunks } }
+  return hunks
 }
+
 async function fetchCommitDiff(owner, repository, baseOid, headOid) {
   const githubApiUrl = `https://api.github.com/repos/${owner}/${repository}/compare/${baseOid}...${headOid}`
 
   console.info("Data :", githubApiUrl)
   try {
-    const response = await axios.get(githubApiUrl)
-    const diffData = gitResponse // response.data
+    const response = TEST ? "" : await axios.get(githubApiUrl)
+    const diffData = TEST ? baseGitResponse : response.data // gitResponse // response.data
 
     const output = diffData.files.map((file) => ({
       changeKind: file.status.toString().toUpperCase(),
